fe9c685e0b74bac54d4a0a294ae7c7aa5d4fe11c
P.C. Shyamshankar <sykora@lucentbeing.com>
2009-08-29 22:24:34 +0530

    Added Extended GCD Functions.

--- skynet/math/numbers.py
+++ skynet/math/numbers.py
@@ -3,6 +3,8 @@
 from functools import reduce
 from operator import mul
 
+## Factorial
+
 # Implementation of standard recursive definition.
 # Slow, and subject to hitting the recursion limit.
 def recursive_factorial(n) :
@@ -28,6 +30,8 @@ from math import factorial as builtin_factorial
 # Use the fastest one.
 factorial = builtin_factorial
 
+## GCD
+
 def recursive_gcd(m, n) :
     '''Computes the GCD (Greatest Common Divisor) of two numbers.'''
 
@@ -42,3 +46,31 @@ def iterative_gcd(m, n) :
     return m
 
 gcd = iterative_gcd
+
+## Extended GCD
+
+def iterative_xgcd(m, n) :
+    '''Returns numbers x, y and g such that g = gcd(m, n) and x*m + y*n == g.'''
+
+    x = last_y = 0
+    y = last_x = 1
+
+    while n :
+        m, n, q = n, m % n, m // n
+
+        last_x, x = x, last_x - q * x
+        last_y, y = y, last_y - q * y
+
+    return (last_x, last_y, m)
+
+def recursive_xgcd(m, n) :
+    '''Returns numbers x, y and g such that g = gcd(m, n) and x*m + y*n == g.'''
+
+    if n == 0 :
+        return (1, 0, m)
+
+    last_x, last_y, g = recursive_xgcd(n, m % n)
+
+    return last_y, last_x - (m // n) * last_y, g
+
+xgcd = iterative_xgcd
