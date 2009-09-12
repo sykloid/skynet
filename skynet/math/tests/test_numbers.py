ebb74758ae836424ed5e46a5e0381b17dab8308c
P.C. Shyamshankar <sykora@lucentbeing.com>
2009-09-12 16:35:12 +0530

    Added tests for generating divisors through cartesian product.

--- skynet/math/tests/test_numbers.py
+++ skynet/math/tests/test_numbers.py
@@ -256,5 +256,27 @@ class FactorizationTest(unittest.TestCase) :
                              numbers.prime_factors_trial_division(n)), 1)
             )
 
+class DivisorsTest(unittest.TestCase) :
+    known_values = {
+        1 : [1],
+        2 : [1, 2],
+        10 : [1, 2, 5, 10],
+        45 : [1, 3, 5, 9, 15, 45],
+        200 : [1, 2, 4, 5, 8, 10, 20, 25, 40, 50, 100, 200],
+        900 : [1, 2, 3, 4, 5, 6, 9, 10, 12, 15, 18, 20, 25, 30, 36, 45, 50, 60,
+               75, 90, 100, 150, 180, 225, 300, 450, 900],
+        1001 : [1, 7, 11, 13, 77, 91, 143, 1001],
+        9996 : [1, 2, 3, 4, 6, 7, 12, 14, 17, 21, 28, 34, 42, 49, 51, 68, 84,
+                98, 102, 119, 147, 196, 204, 238, 294, 357, 476, 588, 714, 833,
+                1428, 1666, 2499, 3332, 4998, 9996],
+        65536 : [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192,
+                 16384, 32768, 65536],
+        131073 : [1, 3, 43691, 131073],
+    }
+
+    def test_divisors_cartesian_product(self) :
+        for n, d in self.known_values.items() :
+            self.assertEqual(d, sorted(list(numbers.divisors_cartesian_product(n))))
+
 if __name__ == '__main__' :
     unittest.main()
