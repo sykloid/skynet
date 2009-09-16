839fc7cfd4f8f16bb7b3795c4ea79dfe1d8803c4
P.C. Shyamshankar <sykora@lucentbeing.com>
2009-09-16 08:05:22 +0530

    Added number_of_partitions.

--- skynet/math/combinatorics.py
+++ skynet/math/combinatorics.py
@@ -3,6 +3,8 @@
 from functools import reduce
 from operator import mul
 
+from skynet.decorators import memoize
+
 def C(n, r) :
     '''Computes the number of combinations of n items taken r at a time.'''
 
@@ -41,3 +43,26 @@ def next_permutation(items) :
         j -= 1
 
     return items
+
+@memoize
+def number_of_partitions(n) :
+    '''Counts the number of ways in which the given number can be expressed as
+    the sum of positive integers.'''
+
+    if n < 0 :
+        return 0
+    if n == 0 :
+        return 1
+
+    total = 0
+
+    for k in range(1, int(n**0.5) + 1) :
+        p = n - k * (3*k - 1) // 2
+        q = n - k * (3*k + 1) // 2
+
+        a = number_of_partitions(p)
+        b = number_of_partitions(q)
+
+        total += (a + b) if k % 2 else -(a + b)
+
+    return total
